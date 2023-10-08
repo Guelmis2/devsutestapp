1df333590d82f7b0dc8dd44f138f37c7269c83ea
Guelmis <guelmisedwards@gmail.com>
2023-10-08 16:33:00 -0400

    Editing output.tf and Adding Output results for AKS Deployment

--- Terraform_IAC/outputs.tf
+++ Terraform_IAC/outputs.tf
@@ -1,3 +1,39 @@
+output "cluster_resource_group" {
+  value = azurerm_kubernetes_cluster.Devsu_AKS.resource_group_name
+}
+
+output "cluster_location" {
+  value = azurerm_kubernetes_cluster.Devsu_AKS.location
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.Devsu_AKS.name
+}
+
+output "cluster_dns_prefix" {
+  value = azurerm_kubernetes_cluster.Devsu_AKS.dns_prefix
+}
+
+output "cluster_nodepool_name" {
+  value = azurerm_kubernetes_cluster.Devsu_AKS.default_node_pool[0].name
+}
+
+output "cluster_nodepool_size" {
+  value = azurerm_kubernetes_cluster.Devsu_AKS.default_node_pool[0].vm_size
+}
+
+output "cluster_nodepool_node_count" {
+  value = azurerm_kubernetes_cluster.Devsu_AKS.default_node_pool[0].node_count
+}
+
+output "cluster_kubernetes_version" {
+  value = azurerm_kubernetes_cluster.Devsu_AKS.kubernetes_version
+}
+
+output "cluster_tags" {
+  value = azurerm_kubernetes_cluster.Devsu_AKS.tags
+}
+
 output "client_key" {
   value = azurerm_kubernetes_cluster.Devsu_AKS.kube_config.0.client_key
   sensitive = true
@@ -20,7 +56,7 @@ output "cluster_username" {
 
 output "cluster_password" {
   value = azurerm_kubernetes_cluster.Devsu_AKS.kube_config.0.password
-  sensitive = true
+  sensitive = true  
 }
 
 output "kube_config" {
